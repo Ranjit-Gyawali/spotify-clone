87f81c0d7cf99cbd47db846393b6c823d49877b6
Ranjit Gyawali <gyawaliranjit87@gmail.com>
2026-01-13 20:38:26 +0545

    Updaed the Range

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("not zero");
+const pills=document.querySelectorAll(".active");
+
+pills.forEach(pill=>{
+    pill.addEventListener('click',()=>{
+
+        pills.forEach(p=>p.classList.remove('active'));
+
+        pills.forEach(p=>p.classList.add('active'));
+    });
+    
+});
+
+
